9c03ea0cf9882af65ae6b48d88eb5c2771aeea85
Dang Bao Chien <chiendb97@gmail.com>
2018-07-20 08:44:45 +0700

    add lesson3

---
+++ chiendb/lesson_03/CIFAR/src/load_data.py
@@ -0,0 +1,41 @@
+import cPickle
+import numpy as np
+
+class LoadData:
+    def __init__(self):
+        dir = '/home/chiendb/Data/cifar-10-batches-py/'
+        self.X_train = self.unpickle(dir + 'data_batch_1')['data'] / 255
+        self.X_test = self.unpickle(dir + 'test_batch')['data'] / 255
+        self.y_train = self.unpickle(dir + 'data_batch_1')['labels']
+        self.y_test = self.unpickle(dir + 'test_batch')['labels']
+        self.index = 0
+        self.batch_size = 200
+        for i in range(2, 6):
+            self.X_train += self.unpickle(dir + 'data_batch_' + str(i))['data']
+            self.y_train += self.unpickle(dir + 'data_batch_' + str(i))['labels']
+
+        self.size = len(self.y_test)
+        # self.y_train = self.vector_to_one_hot(np.array(self.y_train), 10)
+
+    def get_batch(self):
+        if self.index + self.batch_size > self.size:
+            self.index = 0
+        r = np.array(self.X_train[self.index: self.index + self.batch_size, :]), np.array(
+            self.y_train[self.index: self.index + self.batch_size])
+        self.index += self.batch_size
+        return r
+
+    def get_test(self):
+        return np.array(self.X_test), np.array(self.y_test)
+
+    def vector_to_one_hot(self, vector, n_classes):
+        vector = vector.astype(np.int32)
+        m = np.zeros((vector.shape[0], n_classes))
+        m[np.arange(vector.shape[0]), vector] = 1
+        return m
+
+    def unpickle(self, file):
+        with open(file, 'rb') as fo:
+            dict = cPickle.load(fo)
+        return dict
+
---
+++ chiendb/lesson_03/CIFAR/src/main.py
@@ -0,0 +1,71 @@
+import numpy as np
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, precision_recall_fscore_support
+import torch
+import torch.nn as nn
+from matplotlib import pyplot as plt
+from torch.autograd import Variable
+import torch.nn.functional as F
+from load_data import LoadData
+
+
+class Classifier(nn.Module):
+    def __init__(self, h, n_features, n_classes):
+        super(Classifier, self).__init__()
+        self.rl = nn.ReLU()
+        self.l1 = nn.Linear(n_features, h)
+        self.l2 = nn.Linear(h, h)
+        self.l3 = nn.Linear(h, n_classes)
+
+    def forward(self, X):
+        X = self.l1(X)
+        X = self.rl(X)
+        X = self.l2(X)
+        X = self.rl(X)
+        X = self.l3(X)
+        return X
+
+
+epoch = 20000
+h = 1000
+n_features = 3072
+n_classes = 10
+lr = 0.001
+load = LoadData()
+model = Classifier(h=h, n_features=n_features, n_classes=n_classes)
+optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+criterion = nn.CrossEntropyLoss()
+
+log = []
+for e in range(epoch):
+    X_batch, y_batch = load.get_batch()
+    features = Variable(torch.from_numpy(X_batch).type(torch.FloatTensor))
+    target = Variable(torch.from_numpy(y_batch).type(torch.LongTensor))
+    y_hat = model(features)
+    """
+    print(y_hat)
+    _, y_pred = torch.max(y_hat, 0)
+    y_pred = y_pred.data.numpy()
+    print(target)
+    print(y_pred)
+    """
+    loss = criterion(y_hat, target)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    if e % 1000 == 0:
+        _, y_pred = torch.max(y_hat, 1)
+        y_pred = y_pred.data.numpy()
+        acc = accuracy_score(y_batch, y_pred)
+        print(acc)
+        # p, r, f, _ = precision_recall_fscore_support(y_batch, y_pred)
+        # cm = confusion_matrix(y, y_pred)
+        # log.append((e, loss.data[0], f[0], f[1]))
+    if e % 1000 == 0:
+        print('Epoch %d: %f' % (e, loss))
+print('DONE')
+
+"""
+epochs, losses, f0, f1 = zip(*log)
+figure = plt.plot(epochs, losses, 'r-', epochs, f0, 'b-', epochs, f1, 'g-')
+plt.show()
+"""
