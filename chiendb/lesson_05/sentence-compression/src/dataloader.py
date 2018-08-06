a81cabe80d369c1c26adca0be1227d86a89cc854
Dang Bao Chien <chiendb97@gmail.com>
2018-08-06 14:48:51 +0700

    change

--- chiendb/lesson_05/sentence-compression/src/dataloader.py
+++ chiendb/lesson_05/sentence-compression/src/dataloader.py
@@ -1,14 +1,32 @@
 import json
+import numpy as np
 
 
 class DataLoader:
-    def __init__(self, dir_path):
-        self.dir_path = dir_path
+    def __init__(self):
+        self.dir_path = '/home/chiendb/Data/sentence-compression-data/'
+        self.seq_len = 100
+        self.input_dim = 100
+        self.dictionary = {}
+        self.word_matrix = []
+        with open('/home/chiendb/Data/glove.6B/glove.6B.100d.txt', 'r') as f:
+            lines = f.readlines()
+
+        for i, line in enumerate(lines):
+            word = line.split()
+            self.dictionary[word[0]] = i
+            self.word_matrix.append([float(word[i]) for i in range(1, len(word))])
 
     def write_file(self, file_path, arr):
         f = open(file_path, 'w')
         for line in arr:
             f.write('{}\n'.format(line))
+
+    def read_file(self, file_path):
+        with open(file_path, 'r') as f:
+            lines = f.readlines()
+
+        return lines
 
     def crawler(self):
         with open(self.dir_path + 'compression-data.json', 'r') as f:
@@ -24,22 +42,20 @@ class DataLoader:
                 lines += line
             else:
                 js = json.loads(lines)
-                sentence = js['graph']['sentence']
+                node = js['source_tree']['node'][1:]
+                sentence_dict = {word['word'][0]['id']: word['word'][0]['stem'] for word in node}
+                sentence = []
                 compression = js['compression_untransformed']['text']
                 bin = []
-                sentence_id = []
-                node = js['graph']['node'][1:]
-                for words in node:
-                    sentence_id += [word['id'] for word in words['word']]
                 compression_id = [word['child_id'] for word in js['compression_untransformed']['edge']]
-                for i in range(min(sentence_id), max(sentence_id) + 1):
-                    if i in compression_id:
+                for k, v in sentence_dict.items():
+                    sentence.append(v)
+                    if k in compression_id:
                         bin.append('1')
                     else:
                         bin.append('0')
                 bin = ' '.join(bin)
-                if sentence[-1] != '.':
-                    sentence += '.'
+                sentence = ' '.join(sentence)
                 sentences.append(sentence)
                 compressions.append(compression)
                 bins.append(bin)
@@ -55,6 +71,36 @@ class DataLoader:
         self.write_file(self.dir_path + 'valid.bin', bins[8000:9000])
         self.write_file(self.dir_path + 'test.bin', bins[9000:])
 
+    def word2vec(self, lines):
+        X = np.zeros((len(lines), self.seq_len, self.input_dim))
+        for i, line in enumerate(lines):
+            words = line.split()
+            num_words = len(words)
+            for j, word in enumerate(words):
+                X[i][self.seq_len-num_words+j] = self.word_matrix[self.dictionary[word]]
 
-data_loader = DataLoader('/home/chiendb/Data/sentence-compression-data/')
-data_loader.crawler()
+        return X
+
+    def str2int(self, lines):
+        Y = np.zeros((len(lines), self.seq_len))
+        for i, line in enumerate(lines):
+            num = len(line.split())
+            for j, v in enumerate(line.split()):
+                Y[i][self.seq_len-num+j] = v
+
+        return Y
+
+    def get_data(self):
+        features_train = self.word2vec(self.read_file(self.dir_path + 'train.ori'))
+        target_train = self.str2int(self.read_file(self.dir_path + 'train.bin'))
+        features_valid = self.word2vec(self.read_file(self.dir_path + 'valid.ori'))
+        target_valid = self.str2int(self.read_file(self.dir_path + 'valid.bin'))
+        features_test = self.word2vec(self.read_file(self.dir_path + 'test.ori'))
+        target_test = self.str2int(self.read_file(self.dir_path + 'test.bin'))
+        return {'feutures': features_train, 'target': target_train}, \
+               {'feutures': features_valid, 'target': target_valid}, \
+               {'feutures': features_test, 'target': target_test}
+
+
+data_loader = DataLoader()
+# data_loader.crawler()
--- chiendb/lesson_05/sentence-compression/src/models.py
+++ chiendb/lesson_05/sentence-compression/src/models.py
@@ -1,36 +1,28 @@
 from builtins import super
-
 import torch.nn as nn
 
 
-class RNNCell(nn.Module):
-    def __init__(self, input_dim, hidden_dim):
-        super(RNNCell, self).__init__()
+class SentenceCompression(nn.Module):
+    def __init__(self, input_dim, hidden_dim, seq_len, num_classes):
+        super(SentenceCompression, self).__init__()
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
-        self.lx = nn.Linear(input_dim, hidden_dim)
-        self.lh = nn.Linear(hidden_dim, hidden_dim)
-        self.activation = nn.Tanh()
+        self.seq_len = seq_len
+        self.num_classes = num_classes
+        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers=2)
+        self.linear = nn.Linear(self.hidden_dim, self.num_classes)
+        self.action = nn.Sigmoid()
 
-    def forward(self, x, h):
-        h = self.activation(self.lx(x) + self.lh(h))
-        return h
-
-
-class RNN(nn.Module):
-    def __init__(self, input_dim, hidden_dim):
-        super(RNN, self).__init__()
-        self.input_dim = input_dim
-        self.hidden_dim = hidden_dim
-        self.rnn_cell = RNNCell(input_dim, hidden_dim)
-        self.linear = nn.Linear
-
-    def init_hidden(self):
-        pass
-
-    def forward(self, x, h):
-        for xi in x:
-            h = self.rnn_cell(xi, h)
-
-        pass
+    def forward(self, X):
+        # batch*seq_len*input_dim
+        X = X.transpose((1, 0, 2))
+        # seq_len*batch*input_dim
+        X = self.lstm(X)
+        # seq_len*batch*hidden_size
+        X = X.transpose((1, 0, 2))
+        X = [self.linear(Xi) for Xi in X]
+    
+        X = self.linear(X)
+        X = self.action(X)
+        return X
 
